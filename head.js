be6db95adfe586cb1bc5bd68ae417213473b91e0
Malcolm McQueen <malcolmmcqueen@live.com>
2022-08-23 01:03:25 +0000

    added head.js to repo

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected)  {
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+  } else  {
+    console.log(`💀💀💀 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+const head = function(givenArray) {
+  return givenArray[0];
+};
+
+assertEqual(head([]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
