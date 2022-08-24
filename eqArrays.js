08f9bec2a2eb9012f22c536d78c2d63153b4bb99
Malcolm McQueen <malcolmmcqueen@live.com>
2022-08-24 05:05:28 +0000

    created and completed core code for eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected)  {
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+  } else  {
+    console.log(`💀💀💀 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+
+const eqArrays = function(array1, array2) {
+  let output = false
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        output = true
+      } else {
+        output = false;
+        return output;
+      }
+    }
+  } else {
+    return output;
+  }
+  return output;
+};
+
+assertEqual(eqArrays([3, 2 ,1], [1, 2, 3]), true)
