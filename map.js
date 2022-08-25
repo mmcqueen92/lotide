f77c3a2ba917bc9b36bb1de19441a146db60b648
Malcolm McQueen <malcolmmcqueen@live.com>
2022-08-25 23:22:40 +0000

    created and wrote core code for map.js

---
+++ map.js
@@ -0,0 +1,45 @@
+const map = function(array, callback)  {
+  const results = [];
+  for (const element of array) {
+    results.push(callback(element));
+  }
+  return results;
+}
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true)  {
+    return console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    return console.log(`💀💀💀 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let output = false;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        output = true;
+      } else {
+        output = false;
+        return output;
+      }
+    }
+  } else {
+    return output;
+  }
+  return output;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+const numbers = [1, 2, 3, 4, 5];
+const results2 = map(numbers, number => number *3)
+const results3 = map(words, word => word.length)
+
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't'])
+
+assertArraysEqual(results2, [3, 6, 9, 12, 15])
+
+assertArraysEqual(results3, [6, 7, 2, 5, 3])
+
