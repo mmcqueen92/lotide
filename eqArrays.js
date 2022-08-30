cb74a56af3f1e2a83be9e6d95629c12828396c18
Malcolm McQueen <malcolmmcqueen@live.com>
2022-08-30 03:01:23 +0000

    updated eqArrays.js with recursive function to handle nested arrays. created assertArraysEqualTest.js and moved/wrote tests for assertArraysEqual.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,26 @@
-const eqArrays = function(array1, array2) {
+const eqArrays = function (array1, array2) {
+  let output = true;
   if (array1.length !== array2.length) {
-    return false;
+    output = false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    } 
-  } 
-  return true;
+    if (output === true) {
+      if (Array.isArray(array1[i]) === true) {
+        if (Array.isArray(array2[i]) === false) {
+          output = false;
+        }
+        output = eqArrays(array1[i], array2[i]);
+      } else if (array1[i] !== array2[i]) {
+        output = false;
+      }
+    }
+  }
+  return output;
 };
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]]));
+console.log(eqArrays([[2, 4], [4]], [[2, 3], [4]]));
+console.log(eqArrays([[2, 3], 4], [[2, 3], [4]]));
+console.log(eqArrays([2, 3, [4]], [[2, 3], [4]]));
 
 module.exports = eqArrays;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,22 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+const array1 = [1, 2, 3];
+const array2 = [1, 2, 3];
+const array3 = [3, 2, 1];
+const array4 = ['first', 'second', 'third', 'fourth', 'fifth'];
+const array5 = ['first', 'second', 'third', 'fourth', 'fifth'];
+const array6 = ['this', 'array', 'is', 'not', 'the', 'same'];
+const array7 = [[2, 3], [4]]
+const array8 = [[2, 3], [4]]
+const array9 = [[2, 4], [4]]
+const array10 = [[2, 3], 4]
+const array11 = [2, 3, [4]]
+
+assertArraysEqual(array1, array2); // should return pass
+assertArraysEqual(array1, array3); // should return fail
+assertArraysEqual(array4, array5); // should return pass
+assertArraysEqual(array5, array6); // should return fail
+assertArraysEqual(array7, array8); // should return pass
+assertArraysEqual(array7, array9); // should return fail
+assertArraysEqual(array7, array10); // should return fail
+assertArraysEqual(array7, array11); // should return fail
