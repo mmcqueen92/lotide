20fbcdb7d8708a0d642c8b5f15d4e93eea005c04
Malcolm McQueen <malcolmmcqueen@live.com>
2022-08-30 03:16:52 +0000

    created middleTest.js and moved test cases from middle.js to middleTest.js

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    } 
-  } 
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2) === true)  {
-    return console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    return console.log(`💀💀💀 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 const middle = function(givenArray) {
   if (givenArray.length < 3) {
     //Array does not have a middle element, return empty array
@@ -32,7 +12,6 @@ const middle = function(givenArray) {
   }
 };
 
-const testArray = [1, 2, 3, 4, 5, 6, 7, 8];
 
-assertArraysEqual(middle(testArray), [4, 5]);
 
+module.exports = middle;
